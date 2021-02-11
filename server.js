c7bb2d3391bb01c1f8993dc67c52d0b6b7e8b666
William Payne <payne.j.will@gmail.com>
2021-02-11 14:06:30 -0600

    fix

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use((error, req, res, next) => {
   handleError(error, res);
 });
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
