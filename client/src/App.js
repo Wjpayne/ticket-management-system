84cdad5afb07180695e0052e1ad2c416e7b683cd
William Payne <payne.j.will@gmail.com>
2021-02-11 15:36:45 -0600

    fix routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ import { AddTicket } from "./Pages/AddTicket/AddTicket";
 import { TicketList } from "./Pages/TicketList";
 import { Dashboard } from "./Pages/Dashboard/Dashboard";
 import { TicketPage } from "./Pages/TicketPage/TicketPage";
-import { BrowserRouter, Switch, Route, withRouter, HashRouter } from "react-router-dom";
+import { Switch, Route, withRouter, HashRouter } from "react-router-dom";
 import { PrivateRoute } from "./Components/PrivateRoutes/PrivateRoute";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const AnimatedSwitch = withRouter(({location}) => (
   <TransitionGroup>
-    <CSSTransition key = {location.key} timeout = {700} classNames = "slide">
+    <CSSTransition key = {location.pathname} timeout = {700} classNames = "slide">
   <Switch location = {location}>
   <Route exact path="/">
     <LandingPage />
