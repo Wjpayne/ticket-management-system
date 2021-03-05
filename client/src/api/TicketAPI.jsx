0980a1d7084ccdd3741c784d92ee07752f79ddb3
William Payne <payne.j.will@gmail.com>
2021-03-05 09:17:04 -0600

    fix local

--- client/src/api/TicketAPI.jsx
+++ client/src/api/TicketAPI.jsx
@@ -3,105 +3,10 @@ import axios from "axios";
 
 
 
-// export const getAllTickets = () => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const result = await axios.get("/ticket", {
-//         headers: {
-//           Authorization: sessionStorage.getItem("accessJWT"),
-//         },
-//       });
-
-//       resolve(result);
-//     } catch (error) {
-//       reject(error);
-//     }
-//   });
-// };
-
-// export const getSingleTicket = (_id) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const result = await axios.get("/ticket/" + _id, {
-//         headers: {
-//           Authorization: sessionStorage.getItem("accessJWT"),
-//         },
-//       });
-
-//       resolve(result);
-//     } catch (error) {
-//       console.log(error.message);
-//       reject(error);
-//     }
-//   });
-// };
-
-// export const updateReplyTicket = (_id, msgObj) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const result = await axios.put("/ticket/" + _id, msgObj, {
-//         headers: {
-//           Authorization: sessionStorage.getItem("accessJWT"),
-//         },
-//       });
-
-//       resolve(result.data);
-//     } catch (error) {
-//       console.log(error.message);
-//       reject(error);
-//     }
-//   });
-// };
-
-// export const updateTicketStatusClosed = (_id) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const result = await axios.patch(
-//         "/ticket/close-ticket/" + _id,
-//         {},
-//         {
-//           headers: {
-//             Authorization: sessionStorage.getItem("accessJWT"),
-//           },
-//         }
-//       );
-
-//       resolve(result.data);
-//     } catch (error) {
-//       console.log(error.message);
-//       reject(error);
-//     }
-//   });
-// };
-
-// export const createNewTicket = (frmData) => {
-//   console.log("from api", frmData);
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const result = await axios.post("/ticket/addticket", frmData, {
-//         headers: {
-//           Authorization: sessionStorage.getItem("accessJWT"),
-//         },
-//       });
-
-//       resolve(result.data);
-//     } catch (error) {
-//       console.log(error.message);
-//       reject(error);
-//     }
-//   });
-// };
-
-// LOCAL
-
-const rootUrl = "http://localhost:5000"
-const ticketUrl = rootUrl + "/ticket/";
-const closeTicketUrl = rootUrl + "/ticket/close-ticket/";
-
 export const getAllTickets = () => {
   return new Promise(async (resolve, reject) => {
     try {
-      const result = await axios.get(ticketUrl, {
+      const result = await axios.get("/ticket", {
         headers: {
           Authorization: sessionStorage.getItem("accessJWT"),
         },
@@ -117,7 +22,7 @@ export const getAllTickets = () => {
 export const getSingleTicket = (_id) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const result = await axios.get(ticketUrl+ _id, {
+      const result = await axios.get("/ticket/" + _id, {
         headers: {
           Authorization: sessionStorage.getItem("accessJWT"),
         },
@@ -134,7 +39,7 @@ export const getSingleTicket = (_id) => {
 export const updateReplyTicket = (_id, msgObj) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const result = await axios.put(ticketUrl+ _id, msgObj, {
+      const result = await axios.put("/ticket/" + _id, msgObj, {
         headers: {
           Authorization: sessionStorage.getItem("accessJWT"),
         },
@@ -152,7 +57,7 @@ export const updateTicketStatusClosed = (_id) => {
   return new Promise(async (resolve, reject) => {
     try {
       const result = await axios.patch(
-        closeTicketUrl + _id,
+        "/ticket/close-ticket/" + _id,
         {},
         {
           headers: {
@@ -169,11 +74,11 @@ export const updateTicketStatusClosed = (_id) => {
   });
 };
 
-export const createNewTicket = (formData) => {
-  console.log("from api", formData);
+export const createNewTicket = (frmData) => {
+  console.log("from api", frmData);
   return new Promise(async (resolve, reject) => {
     try {
-      const result = await axios.post(ticketUrl + "/addticket", formData, {
+      const result = await axios.post("/ticket/addticket", frmData, {
         headers: {
           Authorization: sessionStorage.getItem("accessJWT"),
         },
@@ -186,3 +91,98 @@ export const createNewTicket = (formData) => {
     }
   });
 };
+
+// LOCAL
+
+// const rootUrl = "http://localhost:5000"
+// const ticketUrl = rootUrl + "/ticket/";
+// const closeTicketUrl = rootUrl + "/ticket/close-ticket/";
+
+// export const getAllTickets = () => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const result = await axios.get(ticketUrl, {
+//         headers: {
+//           Authorization: sessionStorage.getItem("accessJWT"),
+//         },
+//       });
+
+//       resolve(result);
+//     } catch (error) {
+//       reject(error);
+//     }
+//   });
+// };
+
+// export const getSingleTicket = (_id) => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const result = await axios.get(ticketUrl+ _id, {
+//         headers: {
+//           Authorization: sessionStorage.getItem("accessJWT"),
+//         },
+//       });
+
+//       resolve(result);
+//     } catch (error) {
+//       console.log(error.message);
+//       reject(error);
+//     }
+//   });
+// };
+
+// export const updateReplyTicket = (_id, msgObj) => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const result = await axios.put(ticketUrl+ _id, msgObj, {
+//         headers: {
+//           Authorization: sessionStorage.getItem("accessJWT"),
+//         },
+//       });
+
+//       resolve(result.data);
+//     } catch (error) {
+//       console.log(error.message);
+//       reject(error);
+//     }
+//   });
+// };
+
+// export const updateTicketStatusClosed = (_id) => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const result = await axios.patch(
+//         closeTicketUrl + _id,
+//         {},
+//         {
+//           headers: {
+//             Authorization: sessionStorage.getItem("accessJWT"),
+//           },
+//         }
+//       );
+
+//       resolve(result.data);
+//     } catch (error) {
+//       console.log(error.message);
+//       reject(error);
+//     }
+//   });
+// };
+
+// export const createNewTicket = (formData) => {
+//   console.log("from api", formData);
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const result = await axios.post(ticketUrl + "/addticket", formData, {
+//         headers: {
+//           Authorization: sessionStorage.getItem("accessJWT"),
+//         },
+//       });
+
+//       resolve(result.data);
+//     } catch (error) {
+//       console.log(error.message);
+//       reject(error);
+//     }
+//   });
+// };
--- client/src/api/userAPI.jsx
+++ client/src/api/userAPI.jsx
@@ -3,91 +3,10 @@ import axios from "axios";
 
 
 
-// export const userLogin = (frmData) => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const res = await axios.post("/user/login", frmData);
-
-//       resolve(res.data);
-
-//       if (res.data.status === "success") {
-//         sessionStorage.setItem("accessJWT", res.data.createToken);
-//         localStorage.setItem(
-//           "crmSite",
-//           JSON.stringify({ refreshJWT: res.data.refreshToken })
-//         );
-//       }
-//     } catch (error) {
-//       reject(error);
-//     }
-//   });
-// };
-
-// export const fetchUser = () => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const accessJWT = sessionStorage.getItem("accessJWT");
-//       if (!accessJWT) {
-//         reject("Token not found");
-//       }
-//       const res = await axios.get("/user/get", {
-//         headers: { Authorization: accessJWT },
-//       });
-
-//       resolve(res.data);
-//     } catch (error) {
-//       reject(error.message);
-//     }
-//   });
-// };
-
-// export const fetchNewAccessToken = () => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const { refreshJWT } = JSON.parse(localStorage.getItem("crmSite"));
-//       if (!refreshJWT) {
-//         reject("Token not found");
-//       }
-//       const res = await axios.get("/user/token", {
-//         headers: { Authorization: refreshJWT },
-//       });
-
-//       if (res.data.status === "success") {
-//         sessionStorage.setItem("accessJWT", res.data.createToken);
-//       }
-
-//       resolve(true);
-//     } catch (error) {
-//       if (error.message === "Request failed with status code 403")
-//         localStorage.removeItem("crmSite");
-//       reject(false);
-//     }
-//   });
-// };
-
-// export const userLogOut = async () => {
-//   try {
-//     await axios.delete("/user/logout", {
-//       headers: { Authorization: sessionStorage.getItem("accessJWT") },
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-
-
-
-// LOCAL
-
-const rootUrl = "http://localhost:5000/";
-
-const loginUrl = rootUrl + "user/login";
-
 export const userLogin = (frmData) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const res = await axios.post(loginUrl, frmData);
+      const res = await axios.post("/user/login", frmData);
 
       resolve(res.data);
 
@@ -111,7 +30,7 @@ export const fetchUser = () => {
       if (!accessJWT) {
         reject("Token not found");
       }
-      const res = await axios.get(rootUrl + "user/get", {
+      const res = await axios.get("/user/get", {
         headers: { Authorization: accessJWT },
       });
 
@@ -129,7 +48,7 @@ export const fetchNewAccessToken = () => {
       if (!refreshJWT) {
         reject("Token not found");
       }
-      const res = await axios.get(rootUrl + "user/token", {
+      const res = await axios.get("/user/token", {
         headers: { Authorization: refreshJWT },
       });
 
@@ -148,10 +67,91 @@ export const fetchNewAccessToken = () => {
 
 export const userLogOut = async () => {
   try {
-    await axios.delete(rootUrl + "user/logout", {
+    await axios.delete("/user/logout", {
       headers: { Authorization: sessionStorage.getItem("accessJWT") },
     });
   } catch (error) {
     console.log(error);
   }
 };
+
+
+
+
+// LOCAL
+
+// const rootUrl = "http://localhost:5000/";
+
+// const loginUrl = rootUrl + "user/login";
+
+// export const userLogin = (frmData) => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const res = await axios.post(loginUrl, frmData);
+
+//       resolve(res.data);
+
+//       if (res.data.status === "success") {
+//         sessionStorage.setItem("accessJWT", res.data.createToken);
+//         localStorage.setItem(
+//           "crmSite",
+//           JSON.stringify({ refreshJWT: res.data.refreshToken })
+//         );
+//       }
+//     } catch (error) {
+//       reject(error);
+//     }
+//   });
+// };
+
+// export const fetchUser = () => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const accessJWT = sessionStorage.getItem("accessJWT");
+//       if (!accessJWT) {
+//         reject("Token not found");
+//       }
+//       const res = await axios.get(rootUrl + "user/get", {
+//         headers: { Authorization: accessJWT },
+//       });
+
+//       resolve(res.data);
+//     } catch (error) {
+//       reject(error.message);
+//     }
+//   });
+// };
+
+// export const fetchNewAccessToken = () => {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const { refreshJWT } = JSON.parse(localStorage.getItem("crmSite"));
+//       if (!refreshJWT) {
+//         reject("Token not found");
+//       }
+//       const res = await axios.get(rootUrl + "user/token", {
+//         headers: { Authorization: refreshJWT },
+//       });
+
+//       if (res.data.status === "success") {
+//         sessionStorage.setItem("accessJWT", res.data.createToken);
+//       }
+
+//       resolve(true);
+//     } catch (error) {
+//       if (error.message === "Request failed with status code 403")
+//         localStorage.removeItem("crmSite");
+//       reject(false);
+//     }
+//   });
+// };
+
+// export const userLogOut = async () => {
+//   try {
+//     await axios.delete(rootUrl + "user/logout", {
+//       headers: { Authorization: sessionStorage.getItem("accessJWT") },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
